9c4f27792e62e0342c26fca39a118261d1a5d4df
U88478 <chelovek.tutu@gmail.com>
2024-06-02 20:10:34 +0300

    Orders

--- algorithms.py
+++ algorithms.py
@@ -11,40 +11,40 @@ review_map = {
 }
 
 
-def insertion_sort(hotels: List[Dict[str, Any]], key: str) -> Dict[str, Any]:
+def insertion_sort(hotels: List[Dict[str, Any]], key: str, order: int) -> Dict[str, Any]:
     start_time = time.time()
     for i in range(1, len(hotels)):
         current = hotels[i]
         j = i - 1
         while j >= 0 and (review_map[hotels[j][key]] < review_map[current[key]] if key == 'hotel_experience'
-                else hotels[j][key] < current[key]):
+        else hotels[j][key] < current[key]):
             hotels[j + 1] = hotels[j]
             j -= 1
         hotels[j + 1] = current
     end_time = time.time()
     return {
-        'sorted_hotels': hotels[:100],
+        'sorted_hotels': hotels[::-order][:100],
         'time': end_time - start_time
     }
 
 
-def selection_sort(hotels: List[Dict[str, Any]], key: str) -> Dict[str, Any]:
+def selection_sort(hotels: List[Dict[str, Any]], key: str, order: int) -> Dict[str, Any]:
     start_time = time.time()
     for i in range(len(hotels)):
         max_idx = i
         for j in range(i + 1, len(hotels)):
             if (review_map[hotels[j][key]] > review_map[hotels[max_idx][key]] if key == 'hotel_experience'
-                    else hotels[j][key] > hotels[max_idx][key]):
+            else hotels[j][key] > hotels[max_idx][key]):
                 max_idx = j
         hotels[i], hotels[max_idx] = hotels[max_idx], hotels[i]
     end_time = time.time()
     return {
-        'sorted_hotels': hotels[:100],
+        'sorted_hotels': hotels[::-order][:100],
         'time': end_time - start_time
     }
 
 
-def bubble_sort(hotels: List[Dict[str, Any]], key: str) -> Dict[str, Any]:
+def bubble_sort(hotels: List[Dict[str, Any]], key: str, order: int) -> Dict[str, Any]:
     start_time = time.time()
     n = len(hotels)
     for i in range(n):
@@ -54,6 +54,6 @@ def bubble_sort(hotels: List[Dict[str, Any]], key: str) -> Dict[str, Any]:
                 hotels[j], hotels[j + 1] = hotels[j + 1], hotels[j]
     end_time = time.time()
     return {
-        'sorted_hotels': hotels[:100],
+        'sorted_hotels': hotels[::-order][:100],
         'time': end_time - start_time
     }
--- ui.py
+++ ui.py
@@ -14,7 +14,9 @@ class HotelApp(QtWidgets.QMainWindow):
 
         # Connect buttons
         self.SortButton.clicked.connect(self.sort_hotels)
+        self.DownloadButton.clicked.connect(self.download_results)
         self.PropertiesComboBox.currentTextChanged.connect(self.on_criterion_change)
+        self.OrderBox.currentTextChanged.connect(self.on_order_change)
         print("Buttons connected")
 
         # Load hotel data
@@ -23,8 +25,10 @@ class HotelApp(QtWidgets.QMainWindow):
         print(f"Loaded {len(self.hotels)} hotels")
 
         # Initialize UI
-        self.sort_algorithm = "Insertion Sort"
+        self.sort_algorithm = ""
         self.sort_criterion = ""
+        self.selected_amenities = []
+        self.order = -1
         self.execution_time = ""
 
         # Load amenities from file
@@ -52,6 +56,9 @@ class HotelApp(QtWidgets.QMainWindow):
         except Exception as e:
             print(f"Error loading amenities: {e}")
 
+    def on_order_change(self):
+        self.order = -1 if self.OrderBox.currentText() == "Descending" else 1
+
     def on_criterion_change(self):
         if self.PropertiesComboBox.currentText() == "Amenities":
             self.scrollArea.setVisible(True)
@@ -60,23 +67,24 @@ class HotelApp(QtWidgets.QMainWindow):
             self.scrollArea.setVisible(False)
             self.resize(self.initial_width, self.height())  # Adjust width back to original
 
-    def filter_hotels_by_amenities(self, hotels, selected_amenities):
-        if not selected_amenities:
+    def filter_hotels_by_amenities(self, hotels):
+        if not self.selected_amenities:
             return hotels
-        return [hotel for hotel in hotels if all(amenity in hotel['amenities'] for amenity in selected_amenities)]
+        return [hotel for hotel in hotels if all(amenity in hotel['amenities'] for amenity in self.selected_amenities)]
 
     def sort_hotels(self):
         try:
             # Get selected criteria
-            property_to_sort = self.PropertiesComboBox.currentText()
+            self.sort_algorithm = self.SortComboBox.currentText()
+            self.sort_criterion = self.PropertiesComboBox.currentText()
 
-            print(f"Sorting using {self.sort_algorithm} by {property_to_sort}")
+            print(f"Sorting using {self.sort_algorithm} by {self.sort_criterion} {self.order}")
 
             # Filter hotels if sorting by amenities
-            if property_to_sort == "Amenities":
-                selected_amenities = [amenity for amenity, checkbox in self.amenities_checkboxes.items() if
-                                      checkbox.isChecked()]
-                filtered_hotels = self.filter_hotels_by_amenities(self.hotels, selected_amenities)
+            if self.sort_criterion == "Amenities":
+                self.selected_amenities = [amenity for amenity, checkbox in self.amenities_checkboxes.items() if
+                                           checkbox.isChecked()]
+                filtered_hotels = self.filter_hotels_by_amenities(self.hotels)
                 key = "hotel_name"  # Sort by hotel name after filtering
             else:
                 filtered_hotels = self.hotels
@@ -85,27 +93,30 @@ class HotelApp(QtWidgets.QMainWindow):
                     "Hotel Experience": "hotel_experience",
                     "Price": "price"
                 }
-                key = property_map.get(property_to_sort, 'hotel_rating')
+                key = property_map.get(self.sort_criterion, 'hotel_rating')
 
             # Sort based on the selected algorithm
-            if self.sort_algorithm == "Insertion Sort":
-                result = insertion_sort(filtered_hotels, key)
-            elif self.sort_algorithm == "Selection Sort":
-                result = selection_sort(filtered_hotels, key)
-            elif self.sort_algorithm == "Bubble Sort":
-                result = bubble_sort(filtered_hotels, key)
-            else:
-                raise ValueError(f"Unsupported sort algorithm: {self.sort_algorithm}")
+            match self.sort_algorithm:
+                case "Insertion Sort":
+                    result = insertion_sort(filtered_hotels, key, self.order)
+                case "Selection Sort":
+                    result = selection_sort(filtered_hotels, key, self.order)
+                case "Bubble Sort":
+                    result = bubble_sort(filtered_hotels, key, self.order)
+                case _:
+                    raise ValueError(f"Unsupported sort algorithm: {self.sort_algorithm}")
+
+            self.execution_time = f"{result['time']:.6f}"
+            print(f"Execution time: {self.execution_time}")
 
             # Display results
-            self.display_results(self.sort_algorithm, result, property_to_sort)
+            self.display_results(self.sort_algorithm, result, self.sort_criterion)
         except Exception as e:
             print(f"Error in sort_hotels: {e}")
 
     def display_results(self, sort_algorithm, result, criterion):
         try:
             self.results.clear()
-            self.execution_time = f"{result['time']:.6f}"
             self.ExecutionLabel.setText(f"{sort_algorithm} Execution Time: {self.execution_time} seconds")
 
             html_content = """
@@ -117,11 +128,6 @@ class HotelApp(QtWidgets.QMainWindow):
                     border-bottom: 2px solid #2E8B57;
                     padding-bottom: 10px;
                     margin-bottom: 20px;
-                }
-                ul {
-                    list-style-type: none;
-                    padding: 0;
-                    margin: 0;
                 }
                 li {
                     margin-bottom: 10px;
@@ -145,28 +151,50 @@ class HotelApp(QtWidgets.QMainWindow):
                     color: #555;
                     font-size: 14px;
                 }
+                .highlight {
+                    font-weight: bold;
+                    color: #FF4500;
+                }
             </style>
             <h2>%s</h2>
-            <ul>
+            <ol>
             """ % criterion
 
             for hotel in result['sorted_hotels']:
                 name = hotel['hotel_name']
                 value = hotel[criterion.lower().replace(' ', '_')]
                 if criterion.lower() == "amenities":
-                    value = ", ".join(hotel['amenities'])
+                    value = ", ".join(
+                        [f"<span class='highlight'>{amenity}</span>" if amenity in self.selected_amenities
+                         else amenity for amenity in hotel['amenities']]
+                    )
                 html_content += f"""
                 <li>
                     <b>{name}</b> - <span>{value}</span>
                 </li>
                 """
 
-            html_content += "</ul>"
+            html_content += "</ol>"
 
             self.results.setHtml(html_content)
             print("Results displayed")
         except Exception as e:
             print(f"Error in display_results: {e}")
+
+    def download_results(self):
+        try:
+            results = ((
+                           f"{self.sort_algorithm} by {self.sort_criterion}{self.selected_amenities if self.selected_amenities else ""}"
+                           f" in {"Ascending" if self.order == 1 else "Descending"} order in {self.execution_time}s\n\n")
+                       + "\n".join([result for result in self.results.toPlainText().split("\n")[1:]]))
+            if results:
+                file_name, _ = QtWidgets.QFileDialog.getSaveFileName(self, "Save results", "",
+                                                                     "Text Files (*.txt);;All Files (*)")
+                if file_name:
+                    with open(file_name, 'w') as file:
+                        file.write("".join([result for result in results]))
+        except Exception as e:
+            print(e)
 
 
 if __name__ == "__main__":
